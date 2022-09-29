d59e1741e7fec0607c9af47ba4b5e496a9552737
Matheus Der <matheusder1@hotmail.com>
2022-09-29 18:07:52 -0300

    (sparky): asserting strings

---
+++ Sparky/SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Sparky.NUnitTest;
+
+[TestFixture]
+public class CustomerNUnitTests
+{
+    [Test]
+    public void CombineNames_TwoNames_ReturnFullName()
+    {
+        var customer = new Customer();
+
+        var result = customer.CombineNames("Ab", "Cd");
+
+        Assert.AreEqual("Ab Cd", result);
+        Assert.That(result, Does.Contain(" "));
+        Assert.That(result, Does.StartWith("a").IgnoreCase);
+        Assert.That(result, Does.Match("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+    }
+}
