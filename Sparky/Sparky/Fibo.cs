a84a4455f93b6d5d3c5513ab86e34e3f9e6d8435
Matheus Der <matheusder1@hotmail.com>
2022-09-29 20:45:12 -0300

    (sparky): add tests for fibo

---
+++ Sparky/Sparky/Fibo.cs
@@ -0,0 +1,39 @@
+﻿namespace Sparky;
+
+public class Fibo
+{
+	public Fibo()
+	{
+		Range = 5;
+	}
+
+    public int Range { get; set; }
+
+	public List<int> GetFiboSeries()
+	{
+		var fiboSeries = new List<int>();
+		int a = 0, b = 1;
+
+		if (Range == 0)
+			return fiboSeries;
+		
+		if (Range == 1)
+		{
+			fiboSeries.Add(0);
+			return fiboSeries;
+		}
+
+		fiboSeries.Add(0);
+		fiboSeries.Add(1);
+
+		for(int i = 2; i < Range; i++)
+		{
+			int c = a + b;
+			fiboSeries.Add(c);
+			a = b;
+			b = c;
+		}
+
+		return fiboSeries;
+	}
+}
---
+++ Sparky/SparkyNUnitTest/FiboNUnitTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace Sparky.NUnitTest;
+
+[TestFixture]
+public class FiboNUnitTests
+{
+    private Fibo fibo;
+
+    [SetUp]
+    protected void Setup()
+    {
+        fibo = new Fibo();
+    }
+
+    [Test]
+    public void GetFiboSeries_RangeOne_ShouldReturnFiboSeries()
+    {
+        fibo.Range = 1;
+        var expected = new List<int>() { 0 };
+
+        var result = fibo.GetFiboSeries();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result, Is.Ordered);
+        });
+    }
+
+    [Test]
+    public void GetFiboSeries_RangeSix_ShouldReturnFiboSeries()
+    {
+        fibo.Range = 6;
+        var expected = new List<int>() { 0, 1, 1, 2, 3, 5 };
+
+        var result = fibo.GetFiboSeries();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.EqualTo(expected));
+            Assert.That(result.Count, Is.EqualTo(fibo.Range));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Not.Contain(6));
+        });
+    }
+
+    [Test]
+    public void GetFiboSeries_RangeZero_ShouldReturnEmptyList()
+    {
+        fibo.Range = 0;
+
+        var result = fibo.GetFiboSeries();
+
+        Assert.That(result, Is.Empty);
+    }
+}
