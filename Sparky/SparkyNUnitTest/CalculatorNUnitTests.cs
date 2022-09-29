48bbe81f7aae1419e3cb1dc0e5a5ba9e5ee4bc09
Matheus Der <matheusder1@hotmail.com>
2022-09-29 16:33:44 -0300

    (sparky): add n unit test

---
+++ Sparky/SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Sparky.NUnitTests;
+
+[TestFixture]
+public class CalculatorNUnitTests
+{
+    [Test]
+    public void AddNumbers_TwoInt_GetCorrectAddition()
+    {
+        //Arange
+        var calculatorMSTests = new Calculator();
+
+        //Act
+        var result = calculatorMSTests.AddNumbers(10, 20);
+
+        //Assert
+        Assert.AreEqual(30, result);
+    }
+}
