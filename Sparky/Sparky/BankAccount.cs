8fd7621f3c1f2ab96a9dad3392a4cb1783ff27fa
Matheus Der <matheusder1@hotmail.com>
2022-09-30 13:50:54 -0300

    (sparky): create bank account with dependency

---
+++ Sparky/Sparky/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿namespace Sparky;
+
+public class BankAccount
+{
+    public int Balance { private get; set; }
+
+    private readonly ILogBook _logBook;
+
+    public BankAccount(ILogBook logBook)
+    {
+        Balance = 0;
+        _logBook = logBook;
+    }
+
+    public bool Deposit(int amount)
+    {
+        _logBook.Message("Deposit Invoked");
+
+        Balance += amount;
+        return true;
+    }
+
+    public bool Withdraw(int amount)
+    {
+        if(Balance >= amount)
+        {
+            Balance -= amount;
+            return true;
+        }
+
+        return false;
+    }
+
+    public int GetBalance() 
+        => Balance;
+}
