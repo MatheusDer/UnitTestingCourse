9d4253d554964ffa1ef4dfc1a9da3ecfe9a06c2e
Matheus Der <matheusder1@hotmail.com>
2022-10-01 17:37:55 -0300

    (bongo): create unit tests for models

---
+++ Bongo/Bongo.Models.Test/DateInFutureAttributeTests.cs
@@ -0,0 +1,27 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+
+namespace Bongo.Models.Test;
+
+[TestFixture]
+public class DateInFutureAttributeTests
+{
+    [Test]
+    [TestCase(1, ExpectedResult = true)]
+    [TestCase(-1, ExpectedResult = false)]
+    [TestCase(0, ExpectedResult = false)]
+    public bool DateValidator_ReturnsValidity_WhenExpectedDate(int seconds)
+    {
+        var atrribute = new DateInFutureAttribute();
+
+        return atrribute.IsValid(DateTime.Now.AddSeconds(seconds));
+    }
+
+    [Test]
+    public void DateValidator_ReturnsErrorMessage()
+    {
+        var atrribute = new DateInFutureAttribute();
+
+        Assert.That(atrribute.ErrorMessage, Is.EqualTo("Date must be in the future"));
+    }
+}
---
+++ Bongo/Bongo.Models.Tests/DateInFutureAttributeTests.cs
@@ -0,0 +1,13 @@
+﻿using Bongo.Models.ModelValidations;
+using Xunit;
+
+namespace Bongo.Models;
+
+public class DateInFutureAttributeTests
+{
+    [Fact]
+    public void DateValidator_ReturnsValid_WhenDateIsValid()
+    {
+        var dateInFutureAttribute = new DateInFutureAttribute();
+    }
+}
